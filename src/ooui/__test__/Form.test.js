b2e2ea186908d798c39377dd17a556c535f8db5e
Cristian Peraferrer <cperaferrer@gisce.net>
2020-11-23 17:02:27 +0100

    Add form tests

--- src/ooui/__test__/Form.test.js
+++ src/ooui/__test__/Form.test.js
@@ -1,6 +1,6 @@
-import Form from '../Form';
-import Widget from '../Widget';
-
+import Form from "../Form";
+import Widget from "../Widget";
+import Notebook from "../Notebook";
 
 const XML_VIEW_FORM = `<?xml version="1.0"?>
 <form string="Partner Address">
@@ -63,171 +63,196 @@ const XML_VIEW_FORM = `<?xml version="1.0"?>
 `;
 
 const FIELDS = {
-    'aclarador': {
-        'size': 256,
-        'string': 'Aclarador',
-        'type': 'char',
-        'views': {}
-    },
-    'apartat_correus': {
-        'size': 5,
-        'string': 'Apartat de Correus',
-        'type': 'char',
-        'views': {}
-    },
-    'bq': { 'size': 4, 'string': 'Bloc', 'type': 'char', 'views': {} },
-    'country_id': {
-        'context': '',
-        'domain': [],
-        'relation': 'res.country',
-        'size': 64,
-        'string': 'Country',
-        'type': 'many2one',
-        'views': {}
-    },
-    'email': { 'size': 240, 'string': 'E-Mail', 'type': 'char', 'views': {} },
-    'es': { 'size': 10, 'string': 'Escala', 'type': 'char', 'views': {} },
-    'fax': { 'size': 64, 'string': 'Fax', 'type': 'char', 'views': {} },
-    'function': {
-        'context': '',
-        'domain': [],
-        'relation': 'res.partner.function',
-        'size': 64,
-        'string': 'Function',
-        'type': 'many2one',
-        'views': {}
-    },
-    'id_municipi': {
-        'context': '',
-        'domain': [],
-        'relation': 'res.municipi',
-        'size': 64,
-        'string': 'Municipi',
-        'type': 'many2one',
-        'views': {}
-    },
-    'id_poblacio': {
-        'context': '',
-        'domain': [],
-        'relation': 'res.poblacio',
-        'size': 64,
-        'string': 'Població',
-        'type': 'many2one',
-        'views': {}
-    },
-    'mobile': { 'size': 64, 'string': 'Mobile', 'type': 'char', 'views': {} },
-    'name': { 'size': 128, 'string': 'Contact Name', 'type': 'char', 'views': {} },
-    'notes': { 'string': 'Notes', 'type': 'text', 'views': {} },
-    'nv': { 'size': 256, 'string': 'Carrer', 'type': 'char', 'views': {} },
-    'partner_id': {
-        'context': '',
-        'domain': [],
-        'help': 'Keep empty for a private address, not related to partner.',
-        'relation': 'res.partner',
-        'select': true,
-        'size': 64,
-        'string': 'Partner',
-        'type': 'many2one',
-        'views': {}
-    },
-    'phone': { 'size': 64, 'string': 'Phone', 'type': 'char', 'views': {} },
-    'pnp': { 'size': 10, 'string': 'Número', 'type': 'char', 'views': {} },
-    'pt': { 'size': 10, 'string': 'Planta', 'type': 'char', 'views': {} },
-    'pu': { 'size': 10, 'string': 'Porta', 'type': 'char', 'views': {} },
-    'ref_catastral': {
-        'size': 20,
-        'string': 'Ref Catastral (c)',
-        'type': 'char',
-        'views': {}
-    },
-    'state_id': {
-        'context': '',
-        'domain': "[('country_id','=',country_id)]",
-        'relation': 'res.country.state',
-        'size': 64,
-        'string': 'Fed. State',
-        'type': 'many2one',
-        'views': {}
-    },
-    'street': {
-        'digits': [16, 2],
-        'readonly': 1,
-        'size': 128,
-        'string': 'Street',
-        'type': 'char',
-        'views': {}
-    },
-    'street2': { 'size': 128, 'string': 'Street2', 'type': 'char', 'views': {} },
-    'title': {
-        'selection': [['Ms.', 'Madam'],
-        ['Mss', 'Miss'],
-        ['M.', 'Sir'],
-        ['', '']],
-        'size': 32,
-        'string': 'Title',
-        'type': 'selection',
-        'views': {}
-    },
-    'tv': {
-        'context': '',
-        'domain': [],
-        'relation': 'res.tipovia',
-        'size': 64,
-        'string': 'Tipus Via',
-        'type': 'many2one',
-        'views': {}
-    },
-    'type': {
-        'help': 'Used to select automatically the right address according to the context in sales and purchases documents.',
-        'selection': [['default', 'Default'],
-        ['invoice', 'Invoice'],
-        ['delivery', 'Delivery'],
-        ['contact', 'Contact'],
-        ['other', 'Other'],
-        ['ov', 'Oficina Virtual']],
-        'string': 'Address Type',
-        'type': 'selection',
-        'views': {}
-    },
-    'zip': {
-        'change_default': true,
-        'size': 24,
-        'string': 'Zip',
-        'type': 'char',
-        'views': {}
-    }
+  aclarador: {
+    size: 256,
+    string: "Aclarador",
+    type: "char",
+    views: {},
+  },
+  apartat_correus: {
+    size: 5,
+    string: "Apartat de Correus",
+    type: "char",
+    views: {},
+  },
+  bq: { size: 4, string: "Bloc", type: "char", views: {} },
+  country_id: {
+    context: "",
+    domain: [],
+    relation: "res.country",
+    size: 64,
+    string: "Country",
+    type: "many2one",
+    views: {},
+  },
+  email: { size: 240, string: "E-Mail", type: "char", views: {} },
+  es: { size: 10, string: "Escala", type: "char", views: {} },
+  fax: { size: 64, string: "Fax", type: "char", views: {} },
+  function: {
+    context: "",
+    domain: [],
+    relation: "res.partner.function",
+    size: 64,
+    string: "Function",
+    type: "many2one",
+    views: {},
+  },
+  id_municipi: {
+    context: "",
+    domain: [],
+    relation: "res.municipi",
+    size: 64,
+    string: "Municipi",
+    type: "many2one",
+    views: {},
+  },
+  id_poblacio: {
+    context: "",
+    domain: [],
+    relation: "res.poblacio",
+    size: 64,
+    string: "Població",
+    type: "many2one",
+    views: {},
+  },
+  mobile: { size: 64, string: "Mobile", type: "char", views: {} },
+  name: { size: 128, string: "Contact Name", type: "char", views: {} },
+  notes: { string: "Notes", type: "text", views: {} },
+  nv: { size: 256, string: "Carrer", type: "char", views: {} },
+  partner_id: {
+    context: "",
+    domain: [],
+    help: "Keep empty for a private address, not related to partner.",
+    relation: "res.partner",
+    select: true,
+    size: 64,
+    string: "Partner",
+    type: "many2one",
+    views: {},
+  },
+  phone: { size: 64, string: "Phone", type: "char", views: {} },
+  pnp: { size: 10, string: "Número", type: "char", views: {} },
+  pt: { size: 10, string: "Planta", type: "char", views: {} },
+  pu: { size: 10, string: "Porta", type: "char", views: {} },
+  ref_catastral: {
+    size: 20,
+    string: "Ref Catastral (c)",
+    type: "char",
+    views: {},
+  },
+  state_id: {
+    context: "",
+    domain: "[('country_id','=',country_id)]",
+    relation: "res.country.state",
+    size: 64,
+    string: "Fed. State",
+    type: "many2one",
+    views: {},
+  },
+  street: {
+    digits: [16, 2],
+    readonly: 1,
+    size: 128,
+    string: "Street",
+    type: "char",
+    views: {},
+  },
+  street2: { size: 128, string: "Street2", type: "char", views: {} },
+  title: {
+    selection: [
+      ["Ms.", "Madam"],
+      ["Mss", "Miss"],
+      ["M.", "Sir"],
+      ["", ""],
+    ],
+    size: 32,
+    string: "Title",
+    type: "selection",
+    views: {},
+  },
+  tv: {
+    context: "",
+    domain: [],
+    relation: "res.tipovia",
+    size: 64,
+    string: "Tipus Via",
+    type: "many2one",
+    views: {},
+  },
+  type: {
+    help:
+      "Used to select automatically the right address according to the context in sales and purchases documents.",
+    selection: [
+      ["default", "Default"],
+      ["invoice", "Invoice"],
+      ["delivery", "Delivery"],
+      ["contact", "Contact"],
+      ["other", "Other"],
+      ["ov", "Oficina Virtual"],
+    ],
+    string: "Address Type",
+    type: "selection",
+    views: {},
+  },
+  zip: {
+    change_default: true,
+    size: 24,
+    string: "Zip",
+    type: "char",
+    views: {},
+  },
 };
 
 function printRow(row, tab) {
-    console.log('-'.repeat(80))
-    row.forEach(el => {
-        const container = el.container || false;
-        const prefix = ' '.repeat(tab);
-        if (container) {
-            console.log(prefix, el.type, el.container.columns);
-            tab = tab + 4;
-            container.rows.forEach(row => {
-                printRow(row, tab)
-            });
-        } else {
-            console.log(prefix, el);
-        }
-    })
+  console.log("-".repeat(80));
+  row.forEach((el) => {
+    const container = el.container || false;
+    const prefix = " ".repeat(tab);
+    if (container) {
+      console.log(prefix, el.type, el.container.columns);
+      tab = tab + 4;
+      container.rows.forEach((row) => {
+        printRow(row, tab);
+      });
+    } else {
+      console.log(prefix, el);
+    }
+  });
 }
 
-describe('A Form', () => {
-    it('should parse xml', () => {
-        const form = new Form(FIELDS);
-        form.parse(XML_VIEW_FORM);
-        form.container.rows.forEach(row => {
-            //    printRow(row, 0);
-        });
-    });
+describe("A Form", () => {
+  it("should parse xml", () => {
+    const form = new Form(FIELDS);
+    form.parse(XML_VIEW_FORM);
+    //form.container.rows.forEach(row => {
+    //    printRow(row, 0);
+    //});
+    expect(form.fields).toBeDefined();
+    expect(form.container.rows.length).toBeGreaterThan(0);
+  });
+
+  it("should be able to fit a widget with greater colspan than page container columns", () => {
+    const fields = {
+      char1: { size: 128, string: "Name", type: "char", views: {} },
+    };
+    const xmlViewForm = `<?xml version="1.0"?>
+    <form string="Form1">
+        <notebook>
+            <page string="Page1" col="8">
+                <field colspan="16" name="char1" />
+            </page>
+        </notebook>
+    </form>`;
+    const form = new Form(fields);
+    form.parse(xmlViewForm);
+
+    expect(form.container.rows[0][0]).toBeInstanceOf(Notebook);
+  });
 });
 
-describe('A Widget', () => {
-    it('should have colspan 1 by default', () => {
-        const widget = new Widget("Button");
-        expect(widget.colspan).toBe(1);
-    });
+describe("A Widget", () => {
+  it("should have colspan 1 by default", () => {
+    const widget = new Widget("Button");
+    expect(widget.colspan).toBe(1);
+  });
 });
