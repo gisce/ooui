de7a8f6a34ab01be3c52019d663937f6977777a7
Marc Güell Segarra <marc@ondori.dev>
2021-02-23 09:52:45 +0100

    Add tests for nolabel by default in buttons

--- src/spec/Button.spec.ts
+++ src/spec/Button.spec.ts
@@ -1,36 +1,58 @@
 import WidgetFactory from "../WidgetFactory";
 
-describe('A Button', () => {
-    it('should have an id corresponding to field name', () => {
-        const widgetFactory = new WidgetFactory();
-        const props = {
-            name: "button1"
-        };
+describe("A Button", () => {
+  it("should have an id corresponding to field name", () => {
+    const widgetFactory = new WidgetFactory();
+    const props = {
+      name: "button1",
+    };
 
-        const widget = widgetFactory.createWidget("button", props);
+    const widget = widgetFactory.createWidget("button", props);
 
-        expect(widget.id).toBe("button1");
-    });
+    expect(widget.id).toBe("button1");
+  });
 
-    it('should have colspan 1 by default (with no label)', () => {
-        const widgetFactory = new WidgetFactory();
-        const props = {
-            name: "button1",
-        };
-        const widget = widgetFactory.createWidget("button", props);
+  it("should have colspan 1 by default (with no label)", () => {
+    const widgetFactory = new WidgetFactory();
+    const props = {
+      name: "button1",
+    };
+    const widget = widgetFactory.createWidget("button", props);
 
-        expect(widget.colspan).toBe(1);
-    });
+    expect(widget.colspan).toBe(1);
+  });
 
-    it('should properly set caption', () => {
-        const widgetFactory = new WidgetFactory();
-        const props = {
-            name: "char1",
-            string: "Button caption"
-        };
-        const widget = widgetFactory.createWidget("button", props);
+  it("should properly set caption", () => {
+    const widgetFactory = new WidgetFactory();
+    const props = {
+      name: "char1",
+      string: "Button caption",
+    };
+    const widget = widgetFactory.createWidget("button", props);
 
-        expect(widget.caption).toBe("Button caption");
-    });
+    expect(widget.caption).toBe("Button caption");
+  });
 
+  it("should properly have nolabel as true by default", () => {
+    const widgetFactory = new WidgetFactory();
+    const props = {
+      name: "char1",
+      string: "Button caption",
+    };
+    const widget = widgetFactory.createWidget("button", props);
+
+    expect(widget.nolabel).toBe(true);
+  });
+
+  it("should properly have nolabel value passed as props", () => {
+    const widgetFactory = new WidgetFactory();
+    const props = {
+      name: "char1",
+      string: "Button caption",
+      nolabel: false,
+    };
+    const widget = widgetFactory.createWidget("button", props);
+
+    expect(widget.nolabel).toBe(false);
+  });
 });
