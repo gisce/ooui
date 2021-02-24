91a53dc161e473321d13ca6b2f6c23d398eedef7
Marc Güell Segarra <marc@ondori.dev>
2021-02-24 10:28:06 +0100

    Add a preceding label for fields without nolabel and control colspan

--- src/Container.ts
+++ src/Container.ts
@@ -1,5 +1,7 @@
+import Field from "./Field";
 import NewLine from "./NewLine";
 import Widget from "./Widget";
+import Label from "./Label";
 
 class Container {
   /**
@@ -68,6 +70,20 @@ class Container {
       // Widget colspan is greater than container columns, so we change widget
       // colspan to fit container columns.
       widget.colspan = this._columns;
+    }
+
+    // For fields without nolabel we add a preceding label widget
+    if (widget instanceof Field && !widget.nolabel) {
+      if (widget.colspan > 1) {
+        widget.colspan -= 1; // We substract one colspan for the corresponding label
+      }
+
+      const label = new Label({
+        name: widget.id + "_label",
+        string: widget.label,
+      });
+
+      this.addWidget(label);
     }
 
     if (widget.colspan <= this.freePosition()) {
