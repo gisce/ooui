67b4a62a71d056dbe9401671448ebed85556141b
Marc Güell Segarra <marc@ondori.dev>
2021-06-18 13:23:03 +0200

    Try catch for unexpected context formats

--- src/helpers/contextParser.ts
+++ src/helpers/contextParser.ts
@@ -5,31 +5,37 @@ export const parseContext = ({
   context?: string;
   values?: any;
 }) => {
-  if (!context) return undefined;
+  // TODO: remove try/catch when we know for sure that all the incoming contexts formats are expected
+  try {
+    if (!context) return undefined;
 
-  if (context.trim().length === 0) {
+    if (context.trim().length === 0) {
+      return undefined;
+    }
+
+    // TODO: maybe this can be accomplished more performant and elegant with regex
+    const strNoWhitespaces = context.replace(/\s/g, "");
+    const strNoClauLeft = strNoWhitespaces.replace(/\{/g, "");
+    const strNoClauRight = strNoClauLeft.replace(/\}/g, "");
+
+    const entryValues = strNoClauRight.split(",");
+    const valuesSplitted = entryValues.map((entry) => {
+      return entry.split(":");
+    });
+
+    const parsedContext: any = {};
+
+    valuesSplitted.forEach((entry) => {
+      if (entry[1].indexOf("'") === -1) {
+        parsedContext[entry[0].replace(/'/g, "")] =
+          values[entry[1]] || undefined;
+      } else {
+        parsedContext[entry[0].replace(/'/g, "")] = entry[1].replace(/'/g, "");
+      }
+    });
+
+    return parsedContext;
+  } catch (e) {
     return undefined;
   }
-
-  // TODO: maybe this can be accomplished more performant and elegant with regex
-  const strNoWhitespaces = context.replace(/\s/g, "");
-  const strNoClauLeft = strNoWhitespaces.replace(/\{/g, "");
-  const strNoClauRight = strNoClauLeft.replace(/\}/g, "");
-
-  const entryValues = strNoClauRight.split(",");
-  const valuesSplitted = entryValues.map((entry) => {
-    return entry.split(":");
-  });
-
-  const parsedContext: any = {};
-
-  valuesSplitted.forEach((entry) => {
-    if (entry[1].indexOf("'") === -1) {
-      parsedContext[entry[0].replace(/'/g, "")] = values[entry[1]] || undefined;
-    } else {
-      parsedContext[entry[0].replace(/'/g, "")] = entry[1].replace(/'/g, "");
-    }
-  });
-
-  return parsedContext;
 };
