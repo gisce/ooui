d23d334f44b57fe79e79602f08762f2643051261
Marc Güell Segarra <marc@ondori.dev>
2022-03-08 17:44:52 +0100

    Add color and icon parsing in Graph

--- src/Graph.ts
+++ src/Graph.ts
@@ -11,11 +11,24 @@ class Graph {
     return this._type;
   }
 
+  _color: string | null = null;
+  get color(): string | null {
+    return this._color;
+  }
+
+  _icon: string | null = null;
+  get icon(): string | null {
+    return this._icon;
+  }
+
   constructor(xml: string) {
     const parser = new DOMParser();
     const view: Document = parser.parseFromString(xml, "text/xml");
     this._string = view.documentElement.getAttribute("string");
     const type = view.documentElement.getAttribute("type");
+
+    this._color = view.documentElement.getAttribute("color");
+    this._icon = view.documentElement.getAttribute("icon");
 
     if (type === "indicator") {
       this._type = "indicator";
