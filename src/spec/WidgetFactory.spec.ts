c9cd23a6f14aec801ddeb20c5504f814669fa51e
Eduard Carrerars <ecarreras@gisce.net>
2022-07-20 16:57:17 +0200

    Add "tags" widget

--- src/spec/WidgetFactory.spec.ts
+++ src/spec/WidgetFactory.spec.ts
@@ -1,6 +1,7 @@
 import Char from "../Char";
 import Button from "../Button";
 import NewLine from "../NewLine";
+import Tags from "../Tags";
 import WidgetFactory from "../WidgetFactory";
 
 
@@ -36,5 +37,13 @@ describe('A WidgetFactory', () => {
         const widget = widgetFactory.createWidget("newline", props);
         expect(widget).toBeInstanceOf(NewLine);
         expect(widget.type).toBe("newline");
+    });
+
+    it('should be able to retrieve Tags type', () => {
+        const widgetFactory = new WidgetFactory();
+        const props = {};
+        const widget = widgetFactory.createWidget("tags", props);
+        expect(widget).toBeInstanceOf(Tags);
+        expect(widget.type).toBe("tags");
     });
 });
