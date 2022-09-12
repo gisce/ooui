8d86ef6cbcd3bd58caf552404e18c6c6d1cf8391
Eduard Carrerars <ecarreras@gisce.net>
2022-09-12 12:28:59 +0200

    Allow icons in Notebook pages

---
+++ src/spec/Page.spec.ts
@@ -0,0 +1,16 @@
+import WidgetFactory from "../WidgetFactory";
+
+describe("A Page", () => {
+  it("should allow an icon property", () => {
+    const widgetFactory = new WidgetFactory();
+    const props = {
+      string: "Page 1",
+      icon: "home"
+    };
+
+    const widget = widgetFactory.createWidget("page", props);
+
+    expect(widget.label).toBe("Page 1");
+    expect(widget.icon).toBe("home")
+  });
+});
