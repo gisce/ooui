9dcca17ebdab2222b04bb733ae4fa238861149d0
Marc Güell Segarra <marc@ondori.dev>
2022-09-19 18:34:36 +0200

    Polyfills and adjustments

--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,24 @@ import * as path from "path";
 import { defineConfig } from "vite";
 import dts from "vite-plugin-dts";
 import peerDepsExternal from "rollup-plugin-peer-deps-external";
+import rollupNodePolyFill from "rollup-plugin-node-polyfills";
 
 export default defineConfig({
+  resolve: {
+    alias: {
+      process: "rollup-plugin-node-polyfills/polyfills/process-es6",
+      util: "rollup-plugin-node-polyfills/polyfills/util",
+      buffer: "rollup-plugin-node-polyfills/polyfills/buffer-es6",
+      stream: "vite-compatible-readable-stream",
+      _stream_duplex: "vite-compatible-readable-stream/lib/_stream_uplex",
+      _stream_passthrough:
+        "vite-compatible-readable-stream/lib/_stream_passthrough",
+      _stream_readable: "vite-compatible-readable-stream/lib/_stream_readable",
+      _stream_writable: "vite-compatible-readable-stream/lib/_stream_writable",
+      _stream_transform:
+        "vite-compatible-readable-stream/lib/_stream_transform",
+    },
+  },
   plugins: [
     peerDepsExternal(),
     dts({
@@ -20,9 +36,8 @@ export default defineConfig({
       fileName: (format) => `ooui.${format}.js`,
     },
     rollupOptions: {
-      external: [
-        "moment"
-      ]
+      external: ["moment"],
+      plugins: [rollupNodePolyFill() as any],
     },
-  }
+  },
 });
