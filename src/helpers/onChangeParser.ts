a6d50c9a18330323d84aaf64e157b7abdb593be2
Marc Güell Segarra <marc@ondori.dev>
2022-11-29 16:04:38 +0100

    Adjustment in on_change parser

--- src/helpers/onChangeParser.ts
+++ src/helpers/onChangeParser.ts
@@ -1,16 +1,13 @@
 const parseOnChange = (onChangeString: string) => {
-  const splitted = onChangeString
-    .replace(/\s/g, "")
-    .replace(")", "")
-    .split("(");
+  const splitted = onChangeString.trim().replace(")", "").split("(");
 
   const method = splitted[0];
   const argsGross = splitted[1];
-  const argsSplitted = argsGross.split(",");
+  const argsSplitted = argsGross.split(",").map((arg) => arg.trim());
 
   return {
     method,
-    args: argsSplitted
+    args: argsSplitted,
   };
 };
 
