1108f779047b961bbfd7138b70a629ab2ae58c0d
Eduard Carrerars <ecarreras@gisce.net>
2024-07-12 14:49:45 +0200

    feat(chart): add y_range attribute to graph

--- src/Graph/Graph.ts
+++ src/Graph/Graph.ts
@@ -1,6 +1,7 @@
 import { ParsedNode } from "../helpers/nodeParser";
 
 export type GraphType = "indicator" | "indicatorField" | "line" | "bar" | "pie";
+export type RangeType = "auto" | "full";
 
 export class Graph {
   _string: string | null = null;
@@ -23,11 +24,22 @@ export class Graph {
     return this._interval;
   }
 
+  _y_range: RangeType = "full";
+  get y_range(): RangeType {
+    return this._y_range;
+  }
+
   constructor(element: ParsedNode) {
     this._string = element.attributes.string || null;
     this._timerange = element.attributes.timerange || null;
     if (element.attributes.interval) {
       this._interval = parseInt(element.attributes.interval);
+    }
+    if (element.attributes.y_range) {
+      const range = element.attributes.y_range;
+      if (range === "auto" || range === "full") {
+        this._y_range = range;
+      }
     }
   }
 }
