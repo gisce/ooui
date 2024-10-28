205eae22324a599c3a844f40622d0e953716f586
Marc Güell Segarra <marc@ondori.dev>
2024-10-28 14:21:41 +0100

    fix: adjustments in json field

--- src/WidgetFactory.ts
+++ src/WidgetFactory.ts
@@ -38,7 +38,7 @@ import Avatar from "./Avatar";
 import Time from "./Time";
 import Alert from "./Alert";
 import Comments from "./Comments";
-import JSONField from "./JSON";
+import JSONField from "./JSONField";
 
 class WidgetFactory {
   /**
@@ -176,13 +176,16 @@ class WidgetFactory {
       case "json":
         this._widgetClass = JSONField;
         break;
-
+      case "arrow_steps":
+        this._widgetClass = JSONField;
+        break;
       default:
         break;
     }
   }
 
   createWidget(type: string, props: any) {
+    this._widgetClass = undefined;
     let finalType = type;
 
     this.setWidgetClass(type);
