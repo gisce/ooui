a5ab911232c205f3e7fb4d38c71a36b67fb98248
Guillem Julià <gjulia@gisce.net>
2024-11-12 12:59:37 +0100

    test: update tests laststep

--- src/spec/Steps.spec.ts
+++ src/spec/Steps.spec.ts
@@ -21,8 +21,17 @@ describe("A Steps widget", () => {
 
     expect(widget.errorField).toBe(null);
   });
+  it("should have default lastStep to true", () => {
+    const widgetFactory = new WidgetFactory();
+    const props = {
+      name: "state",
+    };
+    const widget = widgetFactory.createWidget("steps", props);
 
-  it("should properly set field", () => {
+    expect(widget.lastStep).toBe(true);
+  });
+
+  it("should properly set field errorField", () => {
     const widgetFactory = new WidgetFactory();
     const props = {
       name: "state",
@@ -31,5 +40,16 @@ describe("A Steps widget", () => {
     const widget = widgetFactory.createWidget("steps", props);
 
     expect(widget.errorField).toBe("error");
+  });
+
+  it("should properly set field lastStep with false", () => {
+    const widgetFactory = new WidgetFactory();
+    const props = {
+      name: "state",
+      widget_props: "{'last_step': false}",
+    };
+    const widget = widgetFactory.createWidget("steps", props);
+
+    expect(widget.lastStep).toBe(false);
   });
 });
