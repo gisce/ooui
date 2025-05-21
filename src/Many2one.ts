8f150c6cf15d4d3fd3ba47b0ba94640a3f2c1715
Eduard Carrerars <ecarreras@gisce.net>
2025-05-21 15:48:51 +0200

    feat(Many2One): add showMenu and showSearch options

--- src/Many2one.ts
+++ src/Many2one.ts
@@ -32,6 +32,14 @@ class Many2one extends Field {
     return this.parsedWidgetProps.showFolder ?? true;
   }
 
+  get showSearch(): boolean {
+    return this.parsedWidgetProps.showSearch ?? true;
+  }
+
+  get showMenu(): boolean {
+    return this.parsedWidgetProps.showMenu ?? true;
+  }
+
   constructor(props: any) {
     super(props);
 
