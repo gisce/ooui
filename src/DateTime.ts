fb50672fe9cd7376fabf903af37b26892b2136d6
Marc Güell Segarra <marc@ondori.dev>
2025-05-26 12:50:46 +0200

    feat: adjust timezone property for needed classes

--- src/DateTime.ts
+++ src/DateTime.ts
@@ -1,11 +1,22 @@
-import Date from "./Date";
+import Field from "./Field";
 
 /**
  * DateTime input
  */
-class DateTime extends Date {
-  constructor(props?: any) {
+class DateTime extends Field {
+  _timezone: string | undefined = undefined;
+
+  get timezone(): string | undefined {
+    return this._timezone;
+  }
+
+  set timezone(value: string | undefined) {
+    this._timezone = value;
+  }
+
+  constructor(props?: { timezone?: string } & any) {
     super(props);
+    this._timezone = props?.timezone || undefined;
   }
 }
 
