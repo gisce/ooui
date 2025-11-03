1205795b3df5ec8218fda837737677c06c2c3cf6
Eduard Carrerars <ecarreras@gisce.net>
2025-11-03 12:25:54 +0100

    feat(Image): add width and height properties
    - Add width and height properties with getters/setters
    - Parse width and height from XML attributes in constructor
    - Add tests for width/height parsing including edge cases

--- src/Image.ts
+++ src/Image.ts
@@ -4,8 +4,42 @@ import Field from "./Field";
  * Image base64 field
  */
 class Image extends Field {
+  _width?: number;
+  get width(): number | undefined {
+    return this._width;
+  }
+
+  set width(value: number | undefined) {
+    this._width = value;
+  }
+
+  _height?: number;
+  get height(): number | undefined {
+    return this._height;
+  }
+
+  set height(value: number | undefined) {
+    this._height = value;
+  }
+
   get showControls(): boolean {
     return this.parsedWidgetProps?.showControls ?? true;
+  }
+
+  constructor(props: any) {
+    super(props);
+
+    if (props) {
+      if (props.width) {
+        const parsedWidth = parseInt(props.width);
+        this._width = isNaN(parsedWidth) ? undefined : parsedWidth;
+      }
+
+      if (props.height) {
+        const parsedHeight = parseInt(props.height);
+        this._height = isNaN(parsedHeight) ? undefined : parsedHeight;
+      }
+    }
   }
 }
 
